945a25e951944a39521d0d46bc72639abaaf0cdb
SmallChi <564952747@qq.com>
2018-12-23 23:52:48 +0800

    增加session的发布接口

---
+++ src/JT808.DotNetty/Interfaces/IJT808Publishing.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JT808.DotNetty.Interfaces
+{
+    public interface IJT808Publishing
+    {
+        Task Publish(string topicName,string key,string value);
+    }
+}
---
+++ src/JT808.DotNetty/Interfaces/IJT808SessionPublishing.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JT808.DotNetty.Interfaces
+{
+    public interface IJT808SessionPublishing : IJT808Publishing
+    {
+
+    }
+}
