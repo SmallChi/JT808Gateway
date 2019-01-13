71fa16cd980489ed635881161f0e4f2b4b29f1e6
SmallChi <564952747@qq.com>
2019-01-13 23:24:46 +0800

    添加gprc服务端及实现

---
+++ src/JT808.DotNetty.Admin/JT808.DotNetty.Dashbord.GrpcServer/GrpcImpls/JT808AtomicCounterServiceGrpcImpl.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Grpc.Core;
+using JT808.GrpcDashbord.AtomicCounterGrpcService;
+using JT808.GrpcDashbord.ServiceGrpcBase;
+using static JT808.GrpcDashbord.AtomicCounterGrpcService.AtomicCounterService;
+
+namespace JT808.DotNetty.Dashbord.GrpcServer.GrpcImpls
+{
+    public class JT808AtomicCounterServiceGrpcImpl: AtomicCounterServiceBase
+    {
+        public override Task<AtomicCounterReply> GetTcpAtomicCounter(EmptyRequest request, ServerCallContext context)
+        {
+            AtomicCounterReply atomicCounterReply = new AtomicCounterReply();
+            atomicCounterReply.AtomicCounterInfo = new AtomicCounterInfo
+            {
+                MsgFailCount = 10,
+                MsgSuccessCount = 11111
+            };
+            atomicCounterReply.ResultReply = new ResultReply
+            {
+                Code = ResultReply.Types.StatusCode.Success,
+            };
+            return Task.FromResult(atomicCounterReply);
+        }
+
+        public override Task<AtomicCounterReply> GetUdpAtomicCounter(EmptyRequest request, ServerCallContext context)
+        {
+            AtomicCounterReply atomicCounterReply = new AtomicCounterReply();
+            atomicCounterReply.AtomicCounterInfo = new AtomicCounterInfo
+            {
+                MsgFailCount = 50,
+                MsgSuccessCount = 10000
+            };
+            atomicCounterReply.ResultReply = new ResultReply
+            {
+                Code = ResultReply.Types.StatusCode.Success,
+            };
+            return Task.FromResult(atomicCounterReply);
+        }
+    }
+}
---
+++ src/JT808.DotNetty.Admin/JT808.DotNetty.Dashbord.GrpcServer/Program.cs
@@ -0,0 +1,36 @@
+﻿using Grpc.Core;
+using JT808.DotNetty.Dashbord.GrpcServer.GrpcImpls;
+using JT808.GrpcDashbord.AtomicCounterGrpcService;
+using System;
+using System.Threading;
+using static JT808.GrpcDashbord.AtomicCounterGrpcService.AtomicCounterService;
+
+namespace JT808.DotNetty.Dashbord.GrpcServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var server = new Server
+            {
+                Services = {
+                    BindService(new JT808AtomicCounterServiceGrpcImpl()),
+                },
+                Ports = {
+                    new ServerPort("0.0.0.0", 14000,ServerCredentials.Insecure)
+                }
+            };
+            Console.WriteLine("Google Grpc Starting");
+            foreach (var item in server.Ports)
+            {
+                Console.WriteLine(string.Format("RPC server {0} listening on port {1}", item.Host, item.Port));
+            }
+            server.Start();
+
+            AtomicCounterServiceClient client = new AtomicCounterServiceClient(new Channel("127.0.0.1:14000", ChannelCredentials.Insecure));
+            var result=client.GetTcpAtomicCounter(new GrpcDashbord.ServiceGrpcBase.EmptyRequest());
+            Console.ReadKey();
+            server.ShutdownAsync().Wait();
+        }
+    }
+}
