ecb34e0b378c01ec752d2f71da2f1ab66004fe33
SmallChi(Koike) <564952747@qq.com>
2019-12-23 11:23:11 +0800

    还原kafka项目

---
+++ src/JT808.Gateway.Kafka/Configs/JT808SessionProducerConfig.cs
@@ -0,0 +1,13 @@
+﻿using Confluent.Kafka;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JT808.Gateway.Configs.Kafka
+{
+    public class JT808SessionProducerConfig : JT808ProducerConfig, IOptions<JT808SessionProducerConfig>
+    {
+        JT808SessionProducerConfig IOptions<JT808SessionProducerConfig>.Value => this;
+    }
+}
