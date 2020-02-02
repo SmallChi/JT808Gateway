fd11350e5e66032139eaf7da2b6bc9a7e8d8da8f
smallchi(Koike) <564952747@qq.com>
2020-02-02 20:53:26 +0800

    升级并完善简单demo

---
+++ simples/JT808.Gateway.SimpleServer/Impl/JT808MsgLogging.cs
@@ -0,0 +1,22 @@
+﻿using JT808.Gateway.MsgLogging;
+using JT808.Protocol.Extensions;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JT808.Gateway.SimpleServer.Impl
+{
+    public class JT808MsgLogging : IJT808MsgLogging
+    {
+        private readonly ILogger Logger;
+        public JT808MsgLogging(ILoggerFactory loggerFactory)
+        {
+            Logger = loggerFactory.CreateLogger("JT808MsgLogging");
+        }
+        public void Processor((string TerminalNo, byte[] Data) parameter, JT808MsgLoggingType jT808MsgLoggingType)
+        {
+            Logger.LogDebug($"{jT808MsgLoggingType.ToString()}-{parameter.TerminalNo}-{parameter.Data.ToHexString()}");
+        }
+    }
+}
--- simples/JT808.Gateway.SimpleServer/Program.cs
+++ simples/JT808.Gateway.SimpleServer/Program.cs
@@ -1,5 +1,10 @@
-﻿using JT808.Gateway.InMemoryMQ;
+﻿using JT808.Gateway.Abstractions.Enums;
+using JT808.Gateway.InMemoryMQ;
 using JT808.Gateway.ReplyMessage;
+using JT808.Gateway.MsgLogging;
+using JT808.Gateway.Traffic;
+using JT808.Gateway.MsgIdHandler;
+using JT808.Gateway.SessionNotice;
 using JT808.Protocol;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -8,6 +13,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Threading.Tasks;
+using JT808.Gateway.SimpleServer.Impl;
 
 namespace JT808.Gateway.SimpleServer
 {
@@ -33,8 +39,14 @@ namespace JT808.Gateway.SimpleServer
                     services.AddJT808Configure()                         
                             .AddGateway(hostContext.Configuration)
                             .AddTcp()
-                            .AddServerInMemoryMQ()
+                            .AddServerInMemoryMQ(JT808ConsumerType.MsgIdHandlerConsumer|
+                                                JT808ConsumerType.ReplyMessageConsumer |
+                                                JT808ConsumerType.MsgLoggingConsumer |
+                                                JT808ConsumerType.ReplyMessageLoggingConsumer)
+                            .AddInMemoryMsgIdHandler<JT808MsgIdHandler>()
                             .AddInMemoryReplyMessage()
+                            .AddInMemoryMsgLogging<JT808MsgLogging>()
+                            .AddInMemorySessionNotice()
                             .Builder();
                 });
 
