596d1fa2d556e5b146b99177766c73bc4c8264e3
SmallChi(Koike) <564952747@qq.com>
2021-03-11 10:39:54 +0800

    还原消息处理服务

---
+++ src/JT808.Gateway.Services/JT808.Gateway.MsgIdHandler/JT808MsgIdHandlerExtensions.cs
@@ -0,0 +1,19 @@
+﻿using JT808.Gateway.Abstractions;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JT808.Gateway.MsgIdHandler
+{
+    public static class JT808MsgIdHandlerExtensions
+    {
+        public static IJT808ClientBuilder AddMsgIdHandler<TJT808MsgIdHandler>(this IJT808ClientBuilder jT808ClientBuilder)
+            where TJT808MsgIdHandler: IJT808MsgIdHandler
+        {
+            jT808ClientBuilder.JT808Builder.Services.AddSingleton(typeof(IJT808MsgIdHandler),typeof(TJT808MsgIdHandler));
+            jT808ClientBuilder.JT808Builder.Services.AddHostedService<JT808MsgIdHandlerHostedService>();
+            return jT808ClientBuilder;
+        }
+    }
+}
